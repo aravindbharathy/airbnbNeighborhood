117367ffda38c65666bf8bf650cfc41e3ee7baf4
aravindb <aravindb@umich.edu>
2017-12-10 20:23:54 -0500

    Added marker and zoom-in

--- app/main/airbnb_main.js
+++ app/main/airbnb_main.js
@@ -17,10 +17,79 @@ var map = new google.maps.Map(d3.select("#map").node(), {
   mapTypeId: google.maps.MapTypeId.roadmap
 });
 
+google.maps.Polygon.prototype.getBounds = function() {
+    var bounds = new google.maps.LatLngBounds();
+    this.getPath().forEach(function(element,index){ bounds.extend(element); });
+    return bounds;
+}
+
+google.maps.Polygon.prototype.getApproximateCenter = function() {
+  var boundsHeight = 0,
+      boundsWidth = 0,
+      centerPoint,
+      heightIncr = 0,
+      maxSearchLoops,
+      maxSearchSteps = 10,
+      n = 1,
+      northWest,
+      polygonBounds = this.getBounds(),
+      testPos,
+      widthIncr = 0;
+
+
+  // Get polygon Centroid
+  centerPoint = polygonBounds.getCenter();
+  return centerPoint;
+};
+
 //Create a geoXML parser for KML neighborhood
-var  neighborhoodParser = new geoXML3.parser(
+var infowindow;
+var neighborhoodParser = new geoXML3.parser(
         {
             map: map,
+            singleInfoWindow: 1,
+            createMarker: function (placemark, doc, neighborhood,polygon) {
+
+                var contentString = '<div id="content">'+
+                                    '<div id="siteNotice">'+
+                                    '</div>'+
+                                    '<h1 id="firstHeading" class="firstHeading">'+ neighborhood.name +'</h1>'+
+                                    '<div id="bodyContent">'+
+                                    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                                    'sandstone rock formation in the southern part of the '+
+                                    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                                    'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                                    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                                    'Heritage Site.</p>'+
+                                    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                                    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                                    '(last visited June 22, 2009).</p>'+
+                                    '</div>'+
+                                    '</div>';
+
+                infowindow = new google.maps.InfoWindow({
+                        minWidth:150, 
+                        maxWidth:400,
+                        content: contentString
+                    });
+
+                var marker=new google.maps.Marker({
+                        title: placemark.vars.val.pri_neigh,
+                        position: polygon.getApproximateCenter(),    
+                        map: map
+                    });        
+
+                google.maps.event.addListener(marker, 'mouseover', function() {
+                    infowindow.open(map,marker);
+                });
+
+                 google.maps.event.addListener(marker, 'click', function() {
+                    this.getMap().setCenter(this.getPosition());
+                    this.getMap().setZoom(14);
+                });
+
+                return marker;
+            },
             afterParse:function(docs){
                 if (docs[0].gpolygons.length>0){ 
                     var colorScale = d3.scale.linear().range([0,255]).domain([1,100]);
@@ -31,7 +100,9 @@ var  neighborhoodParser = new geoXML3.parser(
                         var val = Math.floor(Math.random() * 100) + 1;
                         var color = "rgb(" + Math.floor(colorScale(val)) + "," + Math.floor(colorScale(val)) + ",255)";
                         docs[0].placemarks[i].polygon.setOptions({fillColor: color, strokeColor: "#000000", fillOpacity: 0.5, strokeWidth: 10});
-                        neighborhoods.push(new Neighborhood(polygon,id,name,val));
+                        var neighborhood = new Neighborhood(polygon,id,name,val);
+                        neighborhoods.push(neighborhood);
+                        this.createMarker(docs[0].placemarks[i],docs[0],neighborhood,polygon);
                     }
                     console.log(neighborhoods);
                     loadData();
@@ -39,6 +110,7 @@ var  neighborhoodParser = new geoXML3.parser(
                     //[.....]
                 }
             }
+
         });
 
 
